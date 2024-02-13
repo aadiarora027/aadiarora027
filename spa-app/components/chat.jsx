61444af71f2c3df9f5ae201f2129e4583cee7bdf
Aaditya Arora <159626584+aadiarora027@users.noreply.github.com>
2024-02-14 00:33:30 +0100

    Delete spa-app directory

--- spa-app/components/chat.jsx
+++
@@ -1,9 +0,0 @@
-// components/Chat.jsx
-import SendbirdApp from '@sendbird/uikit-react/App';
-const APP_ID = "YOUR_APP_ID";
-const USER_ID = "ID_OF_USER_IN_ABOVE_APP";
-export default () => (
-   <div style={{ height: "100vh", width: "100vw" }}>
-       <SendbirdApp appId={APP_ID} userId={USER_ID} />
-   </div>
-);
